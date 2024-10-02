831556316e72bd503f0c32b1ddf105c7bf190f6d
Ollie1994 <oliwer.carlsson.jobb@gmail.com>
2024-10-02 17:04:53 +0200

    User+trycatch

--- src/main/java/BudgetApp/TryCatch.java
+++ src/main/java/BudgetApp/TryCatch.java
@@ -1,4 +1,5 @@
 package BudgetApp;
-
+// att ha tryCatchen i en egen klass gör det mycket lättare att återanvända kod-
+// och gör det lättare att hålla main så ren som möjligt.
 public class TryCatch {
 }
--- src/main/java/BudgetApp/User.java
+++ src/main/java/BudgetApp/User.java
@@ -1,4 +1,38 @@
 package BudgetApp;
 
 public class User {
+
+    private String name;
+    private String lastName;
+    private String userName; // optional
+    private String password; // optional ifall vi lägger till logga in
+
+    public User(String name, String lastName, String userName, String password) {
+        this.name = name;
+        this.lastName = lastName;
+        this.userName = userName;
+        this.password = password;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public String getName() {return name;}
+    public void setName(String name) {this.name = name;}
+    public String getLastName() {return lastName;}
+    public void setLastName(String lastName) {this.lastName = lastName;}
+    public String getUserName() {return userName;}
+    public void setUserName(String userName) {this.userName = userName;}
+    public String getPassword() {return password;}
+    public void setPassword(String password) {this.password = password;}
 }
